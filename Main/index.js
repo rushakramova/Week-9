93a9677cfcf710b551d8fed05657839440cfd6a5
rushakramova <rushemz@yandex.ru>
2023-03-26 19:32:29 +0400

    add files

---
+++ Main/index.js
@@ -0,0 +1,39 @@
+let nightcleaner = 40000;
+let vocalteacher = 22000;
+console.log(nightcleaner + vocalteacher);
+
+let city = 'Владивосток';
+let num = 2000;
+console.log(city + " " + num);
+
+let messenger = 128;
+let likes = 254;
+let article = 137;
+let youtube = 201;
+let minutes = 60;
+console.log( (messenger + likes + article + youtube) / minutes);
+
+let number = 2;
+let number1 = 5;
+console.log(number ** number1);
+
+let text = "Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log(text);
+
+const greetings = "Привет";
+const name = "Кот";
+console.log( `${greetings}, ${name}`);
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 15;
+console.log('Немного сторис в соцсетях. Осталось минут: '+ time);
+time -=10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву.');
+
+let fahrenheit = 451;
+let celsius = ((fahrenheit - 32)) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);
